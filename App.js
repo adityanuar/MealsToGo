346ad8eb259c4437d8746fd769d1be7e9a9291b6
adit <aditya.roshadi@3equals.io>
2022-09-14 00:52:30 +0700

    rating svg

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar as ExpoStatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View, SafeAreaView, StatusBar} from "react-native";
+import {Searchbar} from "react-native-paper";
+import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
+import {ThemeProvider} from "styled-components/native";
+import {theme} from "./src/infrastructure/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular
+} from "@expo-google-fonts/oswald";
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+  Lato_700Bold
+} from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+    Lato_700Bold
+  });
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen/>
+      </ThemeProvider>
+      <ExpoStatusBar style={"auto"}/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
